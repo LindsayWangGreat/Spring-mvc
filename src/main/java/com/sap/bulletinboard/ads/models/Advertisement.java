6077acef46000b8a0f37db8d44272ddaad560b53
lindsay wang <lindsay.wang01@sap.com>
2018-04-12 12:22:29 +0800

    8.1:add@column @table

--- src/main/java/com/sap/bulletinboard/ads/models/Advertisement.java
+++ src/main/java/com/sap/bulletinboard/ads/models/Advertisement.java
@@ -2,19 +2,23 @@ package com.sap.bulletinboard.ads.models;
 
 import java.beans.ConstructorProperties;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
+@Table(name = "advertisements")
 public class Advertisement {
     //@JsonProperty("title")
     @NotBlank
+    @Column(name = "mytitle")
     private String title;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
