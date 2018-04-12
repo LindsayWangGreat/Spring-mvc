dcd7597ddb701912cef64f5d72244e01aea1af38
lindsay wang <lindsay.wang01@sap.com>
2018-04-12 08:49:56 +0800

    tomcat crash

--- src/main/java/com/sap/bulletinboard/ads/models/Advertisement.java
+++ src/main/java/com/sap/bulletinboard/ads/models/Advertisement.java
@@ -2,15 +2,23 @@ package com.sap.bulletinboard.ads.models;
 
 import java.beans.ConstructorProperties;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-
+@Entity
 public class Advertisement {
     //@JsonProperty("title")
     @NotBlank
     private String title;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     
     public Advertisement() {
         
