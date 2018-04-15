1bbd060146bf7ba6ab9e08113e0d429df0559ddc
lindsay wang <lindsay.wang01@sap.com>
2018-04-15 22:49:50 +0800

    9.3

--- src/main/java/com/sap/bulletinboard/ads/models/Advertisement.java
+++ src/main/java/com/sap/bulletinboard/ads/models/Advertisement.java
@@ -1,6 +1,8 @@
 package com.sap.bulletinboard.ads.models;
 
 import java.beans.ConstructorProperties;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.Map;
 
 import javax.persistence.Column;
@@ -8,9 +10,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
+import org.apache.commons.logging.LogSource;
 import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.internal.util.logging.Log;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -24,6 +30,10 @@ public class Advertisement {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    
+    @Column(updatable = false)
+    private Timestamp createdAt;
+    private Timestamp modifiedAt;
     
     public Advertisement() {
         
@@ -48,4 +58,32 @@ public class Advertisement {
         // TODO Auto-generated method stub
         this.id = id;
     }
+    
+    public Timestamp getCreatedAt() {
+        // TODO Auto-generated method stub
+        if(this.createdAt!=null) {
+            return createdAt;
+        }
+        return null; // use java.util.Date
+    }
+    @PrePersist
+    protected void onPersist() {
+        setCreatedAt(now());
+    }
+    private void setCreatedAt(Timestamp now) {
+        // TODO Auto-generated method stub
+        createdAt = now;
+    }
+    @PreUpdate
+    protected void onUpdate() {
+        this.modifiedAt = now();
+    }
+    protected Timestamp now() {                       // use java.sql.Timestamp
+        return new Timestamp((new Date()).getTime()); // use java.util.Date
+    }
+
+    public Timestamp getUpdatedAt() {
+        // TODO Auto-generated method stub
+        return modifiedAt;
+    } 
 }
